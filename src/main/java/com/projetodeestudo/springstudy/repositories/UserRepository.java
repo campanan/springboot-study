638c037730b27c97621d5d9bb6ff1cc7e10da192
Angelo Campana Neto <netocampana@gmail.com>
2022-02-04 15:29:42 -0300

    Service layer and component registration

--- src/main/java/com/projetodeestudo/springstudy/repositories/UserRepository.java
+++ src/main/java/com/projetodeestudo/springstudy/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.projetodeestudo.springstudy.repositories;
 import com.projetodeestudo.springstudy.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
