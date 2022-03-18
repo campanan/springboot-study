b8e8dc0ca809c31d14961ccc2074325fe23a1fd8
Angelo Campana Neto <netocampana@gmail.com>
2022-03-18 19:03:20 -0300

    Exception handling - delete

---
+++ src/main/java/com/projetodeestudo/springstudy/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.projetodeestudo.springstudy.services.exceptions;
+
+
+public class DatabaseException extends RuntimeException{
+
+    private static final Long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
